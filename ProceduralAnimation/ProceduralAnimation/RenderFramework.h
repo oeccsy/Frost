2308fa00a00743d442918571ec400b8527f34d8f
YunSeong <77562357+oeccsy@users.noreply.github.com>
2025-01-17 00:41:11 +0900

    feat : render framework

---
+++ ProceduralAnimation/ProceduralAnimation/RenderFramework.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <string>
+
+class RenderFramework
+{
+public:
+	RenderFramework();
+	~RenderFramework();
+
+public:
+	void Init(HWND hwnd);
+	void Update();
+	void Render();
+
+private:
+	void RenderBegin();
+	void RenderEnd();
+
+private:
+	void CreateDeviceAndSwapChain();
+	void CreateRenderTargetView();
+	void SetViewport();
+
+private:
+	void CreateGeometry();
+	void CreateInputLayout();
+	void CreateVS();
+	void CreatePS();
+
+	void LoadShaderFromFile(const wstring& path, const string& name, const string& version, ComPtr<ID3DBlob>& blob);
+
+private:
+	HWND _hwnd;
+	uint32 _width = 0;
+	uint32 _height = 0;
+
+private:
+	// Device & SwapChain
+	ComPtr<ID3D11Device> _device = nullptr;
+	ComPtr<ID3D11DeviceContext> _deviceContext = nullptr;
+	ComPtr<IDXGISwapChain> _swapChain = nullptr;
+
+	// Render Target View
+	ComPtr<ID3D11RenderTargetView> _renderTargetView;
+
+	// Misc
+	D3D11_VIEWPORT _viewport = { 0 };
+	float _clearColor[4] = { 0.5f, 0.5f, 0.5f, 1.f };
+
+private:
+	// Geometry
+	vector<Vertex> _vertices;
+	ComPtr<ID3D11Buffer> _vertexBuffer = nullptr;
+	ComPtr<ID3D11InputLayout> _inputLayout = nullptr;
+
+	// VS
+	ComPtr<ID3D11VertexShader> _vertexShader = nullptr;
+	ComPtr<ID3DBlob> _vsBlob = nullptr;
+
+	// PS
+	ComPtr<ID3D11PixelShader> _pixelShader = nullptr;
+	ComPtr<ID3DBlob> _psBlob = nullptr;
+};
---
+++ ProceduralAnimation/ProceduralAnimation/pch.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "Types.h"
+#include "Values.h"
+#include "Struct.h"
+
+// STL
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+// WIN
+#include <Windows.h>
+#include <assert.h>
+
+// DX
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <wrl.h>
+#include <DirectXMath.h>
+#include <DirectXTex.h>
+#include <DirectXTex.inl>
+using namespace DirectX;
+using namespace Microsoft::WRL;
+
+#pragma comment(lib, "d3d11.lib")
+#pragma	comment(lib, "d3dcompiler.lib")
+
+#ifdef _DEBUG
+#pragma comment(lib, "DirectXTex\\DirectXTex64d.lib")
+#else
+#pragma comment(lib, "DirectXTex\\DirectXTex64d.lib")
+#endif
