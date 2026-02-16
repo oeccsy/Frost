a51fd8b607e661b2d38bb73181680dabd44eb2b6
YunSeong <77562357+oeccsy@users.noreply.github.com>
2026-02-17 03:57:16 +0900

    feat : Engine

--- Engine/Core/Engine.cpp
+++ Engine/Core/Engine.cpp
@@ -1,162 +1,93 @@
 ﻿#include "Engine.h"
+#include "Input.h"
 #include <iostream>
-#include <Windows.h>
-
-#include "Level/Level.h"
-#include "Utils/Utils.h"
 
 Engine* Engine::instance = nullptr;
-
-BOOL WINAPI ConsoleMessageProcedure(DWORD CtrlType)
-{
-	switch (CtrlType)
-	{
-	case CTRL_CLOSE_EVENT:
-		Engine::Get().CleanUp();
-		return false;
-	}
-
-	return false;
-}
 
 Engine::Engine()
 {
 	instance = this;
+	input = std::make_shared<Input>();
+	LoadEngineSettings();
 
 	CONSOLE_CURSOR_INFO info;
 	info.bVisible = false;
 	info.dwSize = 1;
 
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
-	SetConsoleCtrlHandler(ConsoleMessageProcedure, TRUE);
-
-	LoadEngineSettings();
 
 	srand(static_cast<unsigned int>(time(nullptr)));
 }
 
-Engine::~Engine()
-{
-	CleanUp();
-}
-
-void Engine::Run()
-{
-	LARGE_INTEGER currentTime;
-	LARGE_INTEGER previousTime;
-	QueryPerformanceCounter(&currentTime);
-	previousTime = currentTime;
-
-	LARGE_INTEGER frequency;
-	QueryPerformanceFrequency(&frequency);
-
-	float targetFrameRate = settings.framerate == 0.0f ? 60.0f : settings.framerate;
-	float oneFrameTime = 1.0f / targetFrameRate;
-
-	while (true)
-	{
-		if (isQuit) break;
-
-		QueryPerformanceCounter(&currentTime);
-
-		float deltaTime = (currentTime.QuadPart - previousTime.QuadPart) / (float)frequency.QuadPart;
-
-		input.ProcessInput();
-
-		if (deltaTime >= oneFrameTime)
-		{
-			BeginPlay();
-			Tick(deltaTime);
-			Render();
-
-			char fpsText[50] = { };
-			sprintf_s(fpsText, 50, "FPS : %f", (1.0f / deltaTime));
-			SetConsoleTitleA(fpsText);
-
-			previousTime = currentTime;
-
-			input.SavePreviousKeyStates();
-
-			if (mainLevel)
-			{
-				mainLevel->ProcessAddAndDestroyActors();
-			}
-		}
-	}
-
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-}
-
-void Engine::AddLevel(Level* newLevel)
-{
-	SafeDelete(mainLevel);
-	mainLevel = newLevel;
-}
-
-void Engine::CleanUp()
-{
-	SafeDelete(mainLevel);
-}
-
-void Engine::Quit()
-{
-	isQuit = true;
-}
+Engine::~Engine() { }
 
 Engine& Engine::Get()
 {
 	return *instance;
 }
 
-int Engine::Width() const
+void Engine::Run()
 {
-	return settings.width;
+	LARGE_INTEGER current_time;
+	LARGE_INTEGER previous_time;
+	QueryPerformanceCounter(&current_time);
+	previous_time = current_time;
+
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+
+	float target_framerate = (settings.framerate <= 0.0f) ? 60.0f : settings.framerate;
+	float oneframe_time = 1.0f / target_framerate;
+
+	while (true)
+	{
+		QueryPerformanceCounter(&current_time);
+		float delta_time = (current_time.QuadPart - previous_time.QuadPart) / (float)frequency.QuadPart;
+
+		input->ProcessInput();
+
+		if (delta_time >= oneframe_time)
+		{
+			Awake();
+			Update(delta_time);
+			Render();
+
+			char title_text[50] = { };
+			sprintf_s(title_text, 50, "FPS : %f", (1.0f / delta_time));
+			SetConsoleTitleA(title_text);
+
+			previous_time = current_time;
+
+			input->SavePreviousKeyStates();
+		}
+	}
 }
 
-int Engine::Height() const
+void Engine::Quit()
 {
-	return settings.height;
-}
-
-
-
-void Engine::BeginPlay()
-{
-	if (mainLevel) mainLevel->BeginPlay();
-}
-
-void Engine::Tick(float deltaTime)
-{
-	if (mainLevel) mainLevel->Tick(deltaTime);
-}
-
-void Engine::Render()
-{
-	Utils::SetConsoleTextColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-
-	if (mainLevel) mainLevel->Render();
+	PostQuitMessage(0);
 }
 
 void Engine::LoadEngineSettings()
 {
 	FILE* file = nullptr;
 	fopen_s(&file, "../Settings/EngineSettings.txt", "rt");
-	
+
 	if (file == nullptr)
 	{
 		std::cout << "Failed to load engine settings." << '\n';
 		__debugbreak();
 		return;
 	}
-	
+
 	fseek(file, 0, SEEK_END);
-	size_t fileSize = ftell(file);
-	rewind(file);
+	size_t file_size = ftell(file);
+	fseek(file, 0, SEEK_SET);
 
-	char* buffer = new char[fileSize + 1];
-	memset(buffer, 0, fileSize + 1);
+	char* buffer = new char[file_size + 1];
+	memset(buffer, 0, file_size + 1);
 
-	size_t readSize = fread(buffer, sizeof(char), fileSize, file);
+	size_t read_size = fread(buffer, sizeof(char), file_size, file);
 
 	char* context = nullptr;
 	char* token = nullptr;
@@ -183,7 +114,23 @@ void Engine::LoadEngineSettings()
 		token = strtok_s(nullptr, "\n", &context);
 	}
 
-	SafeDeleteArray(buffer);
+	delete[] buffer;
+	buffer = nullptr;
 
 	fclose(file);
+}
+
+void Engine::Awake()
+{
+
+}
+
+void Engine::Update(float delta_time)
+{
+
+}
+
+void Engine::Render()
+{
+
 }
