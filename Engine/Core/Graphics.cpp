89fd8aededd3b229187633b9fcfcf3fe37945f99
YunSeong <77562357+oeccsy@users.noreply.github.com>
2026-02-17 17:14:50 +0900

    feat : Graphics

---
+++ Engine/Core/Graphics.cpp
@@ -0,0 +1,119 @@
+#include "Graphics.h"
+#include "Engine.h"
+
+ComPtr<ID3D11Device> Graphics::device = nullptr;
+ComPtr<ID3D11DeviceContext> Graphics::device_context = nullptr;
+
+Graphics::Graphics(HWND hwnd, int width, int height) : hwnd(hwnd), width(width), height(height)
+{
+	CreateDeviceAndSwapChain();
+	CreateRenderTargetView();
+	CreateDepthStencilView();
+	SetViewport();
+}
+
+Graphics::~Graphics() { }
+
+void Graphics::RenderBegin()
+{
+	device_context->OMSetRenderTargets(1, render_target_view.GetAddressOf(), depth_stencil_view.Get());
+	device_context->ClearRenderTargetView(render_target_view.Get(), clear_color);
+	device_context->ClearDepthStencilView(depth_stencil_view.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1, 0);
+	device_context->RSSetViewports(1, &viewport);
+}
+
+void Graphics::RenderEnd()
+{
+	HRESULT hr = swap_chain->Present(1, 0);
+	assert(SUCCEEDED(hr));
+}
+
+void Graphics::CreateDeviceAndSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	desc.BufferDesc.Width = width;
+	desc.BufferDesc.Height = height;
+	desc.BufferDesc.RefreshRate.Numerator = 60;
+	desc.BufferDesc.RefreshRate.Denominator = 1;
+	desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	desc.SampleDesc.Count = 1;
+	desc.SampleDesc.Quality = 0;
+	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	desc.BufferCount = 1;
+	desc.OutputWindow = hwnd;
+	desc.Windowed = true;
+	desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+	HRESULT hr = ::D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&desc,
+		swap_chain.GetAddressOf(),
+		device.GetAddressOf(),
+		nullptr,
+		device_context.GetAddressOf()
+	);
+
+	assert(SUCCEEDED(hr));
+}
+
+void Graphics::CreateRenderTargetView()
+{
+	HRESULT hr;
+
+	ComPtr<ID3D11Texture2D> backBuffer = nullptr;
+	hr = swap_chain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)backBuffer.GetAddressOf());
+	assert(SUCCEEDED(hr));
+
+	hr = device->CreateRenderTargetView(backBuffer.Get(), nullptr, render_target_view.GetAddressOf());
+	assert(SUCCEEDED(hr));
+}
+
+void Graphics::CreateDepthStencilView()
+{
+	HRESULT hr;
+
+	D3D11_TEXTURE2D_DESC texture2d_desc = { 0 };
+	ZeroMemory(&texture2d_desc, sizeof(texture2d_desc));
+	texture2d_desc.Width = static_cast<int>(width);
+	texture2d_desc.Height = static_cast<int>(height);
+	texture2d_desc.MipLevels = 1;
+	texture2d_desc.ArraySize = 1;
+	texture2d_desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	texture2d_desc.SampleDesc.Count = 1;
+	texture2d_desc.SampleDesc.Quality = 0;
+	texture2d_desc.Usage = D3D11_USAGE_DEFAULT;
+	texture2d_desc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	texture2d_desc.CPUAccessFlags = 0;
+	texture2d_desc.MiscFlags = 0;
+
+	hr = device->CreateTexture2D(&texture2d_desc, nullptr, depth_stencil_texture.GetAddressOf());
+	assert(SUCCEEDED(hr));
+	
+	D3D11_DEPTH_STENCIL_VIEW_DESC stencil_desc;
+	ZeroMemory(&stencil_desc, sizeof(stencil_desc));
+	stencil_desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	stencil_desc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	stencil_desc.Texture2D.MipSlice = 0;
+
+	hr = device->CreateDepthStencilView(depth_stencil_texture.Get(), &stencil_desc, depth_stencil_view.GetAddressOf());
+	assert(SUCCEEDED(hr));
+}
+
+void Graphics::SetViewport()
+{
+	viewport.TopLeftX = 0.f;
+	viewport.TopLeftY = 0.f;
+	viewport.Width = static_cast<float>(width);
+	viewport.Height = static_cast<float>(height);
+	viewport.MinDepth = 0.f;
+	viewport.MaxDepth = 1.f;
+}
---
+++ Includes/Core/Graphics.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Core.h"
+
+class ENGINE_API Graphics
+{
+public:
+	Graphics(HWND hwnd, int width, int height);
+	~Graphics();
+
+	void RenderBegin();
+	void RenderEnd();
+
+	static ComPtr<ID3D11Device> GetDevice() { return device; }
+	static ComPtr<ID3D11DeviceContext> GetDeviceContext() { return device_context; }
+
+private:
+	void CreateDeviceAndSwapChain();
+	void CreateRenderTargetView();
+	void CreateDepthStencilView();
+	void SetViewport();
+
+private:
+	HWND hwnd;
+	int width;
+	int height;
+
+	ComPtr<IDXGISwapChain> swap_chain;
+	ComPtr<ID3D11RenderTargetView> render_target_view;
+	ComPtr<ID3D11Texture2D> depth_stencil_texture;
+	ComPtr<ID3D11DepthStencilView> depth_stencil_view;
+
+	D3D11_VIEWPORT viewport = { 0 };
+	float clear_color[4] = { 0.1f, 0.1f, 0.1f, 1.f };
+
+	static ComPtr<ID3D11Device> device;
+	static ComPtr<ID3D11DeviceContext> device_context;
+};
