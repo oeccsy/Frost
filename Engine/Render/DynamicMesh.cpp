bfcbbba7a0f543f52effed64608b7d9a751693da
YunSeong <77562357+oeccsy@users.noreply.github.com>
2026-02-21 20:36:29 +0900

    feat : dynamic mesh

---
+++ Engine/Render/DynamicMesh.cpp
@@ -0,0 +1,67 @@
+﻿#include "DynamicMesh.h"
+#include "Graphics.h"
+
+DynamicMesh::DynamicMesh() { }
+
+DynamicMesh::~DynamicMesh() { }
+
+void DynamicMesh::CreateBuffers()
+{
+	auto device = Graphics::GetDevice();
+	
+	{
+		D3D11_BUFFER_DESC desc;
+		ZeroMemory(&desc, sizeof(desc));
+		desc.Usage = D3D11_USAGE_DYNAMIC;
+		desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		desc.ByteWidth = (uint32)(sizeof(Vertex) * 200); // TODO : ũ�� �ʰ� ����
+		desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+		D3D11_SUBRESOURCE_DATA data;
+		ZeroMemory(&data, sizeof(data));
+		data.pSysMem = vertices.data();
+
+		HRESULT hr = device->CreateBuffer(&desc, &data, vertex_buffer.GetAddressOf());
+		assert(SUCCEEDED(hr));
+	}
+	
+	{
+		D3D11_BUFFER_DESC desc;
+		ZeroMemory(&desc, sizeof(desc));
+		desc.Usage = D3D11_USAGE_DYNAMIC;
+		desc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		desc.ByteWidth = (uint32)(sizeof(uint32) * 200);
+		desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+		D3D11_SUBRESOURCE_DATA data;
+		ZeroMemory(&data, sizeof(data));
+		data.pSysMem = indices.data();
+
+		HRESULT hr = device->CreateBuffer(&desc, &data, index_buffer.GetAddressOf());
+		assert(SUCCEEDED(hr));
+	}
+}
+
+void DynamicMesh::UpdateBuffers()
+{
+	{
+		D3D11_MAPPED_SUBRESOURCE subResource;
+		ZeroMemory(&subResource, sizeof(subResource));
+
+		auto deviceContext = Graphics::GetDeviceContext();
+		deviceContext->Map(vertex_buffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &subResource);
+		::memcpy(subResource.pData, vertices.data(), sizeof(Vertex) * vertices.size());
+		deviceContext->Unmap(vertex_buffer.Get(), 0);
+	}
+
+	{
+		D3D11_MAPPED_SUBRESOURCE subResource;
+		ZeroMemory(&subResource, sizeof(subResource));
+
+		auto deviceContext = Graphics::GetDeviceContext();
+		deviceContext->Map(index_buffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &subResource);
+		::memcpy(subResource.pData, indices.data(), sizeof(uint32) * indices.size());
+		deviceContext->Unmap(index_buffer.Get(), 0);
+	}
+}
+
--- Engine/Render/Mesh.h
+++ Engine/Render/Mesh.h
@@ -1,18 +1,20 @@
 #pragma once
 
 #include "Core.h"
+#include "RTTI.h"
 #include "Data/Vertex.h"
 #include "Types.h"
 #include <vector>
 #include <memory>
 
-class ENGINE_API Mesh
+class ENGINE_API Mesh : public RTTI
 {
+	RTTI_DECLARATIONS(Mesh, RTTI)
+	
 public:
 	Mesh();
 	virtual ~Mesh();
 
-public:
 	FORCEINLINE const vector<Vertex>& GetVertices() const { return vertices; }
 	FORCEINLINE const vector<uint32>& GetIndices() const { return indices; }
 	FORCEINLINE D3D11_PRIMITIVE_TOPOLOGY GetTopology() { return topology; }
@@ -21,7 +23,6 @@ public:
 	FORCEINLINE void SetIndices(vector<uint32>& new_indices) { indices = new_indices; }
 	FORCEINLINE void SetTopology(D3D11_PRIMITIVE_TOPOLOGY new_topology) { topology = new_topology; }
 
-public:
 	virtual void CreateBuffers();
 	virtual void UpdateBuffers();
 	void CreateInputLayout(shared_ptr<class Material> material);
