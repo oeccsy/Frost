4b101f5b37455e7add2cacab4f4a298f30793ef0
YunSeong <77562357+oeccsy@users.noreply.github.com>
2026-03-19 00:05:03 +0900

    chore : 프로젝트 정돈
    - .gitignore에서 Assets/**/*.obj 파일을 추적하도록 변경
    - legacy 디렉토리 삭제
    - 프로젝트 빌드 이벤트 수정

--- Legacy/ProceduralAnimation/Component.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "Object.h"
-#include "Component.h"
-
-Component::Component() {}
-
-Component::~Component() {}
--- Legacy/ProceduralAnimation/Values.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "Types.h"
-
-const int32 GWinSizeX = 1920;
-const int32 GWinSizeY = 1080;
